765b861c4023b81151eda7e19a7259d3b5357c26
AntoineDupre <antoine.dupre@maxiv.lu.se>
2019-06-14 16:45:03 +0200

    Pep8

--- sardana2xls/utils.py
+++ sardana2xls/utils.py
@@ -3,7 +3,6 @@ import collections
 
 
 class unique_dict(collections.MutableMapping):
-
     def __init__(self, *args, **kwargs):
         self._store = dict()
         self._inverted = dict()
@@ -55,17 +54,18 @@ class unique_bidict(unique_dict):
 
 def get_elements(instance, db):
     names = db.get_device_name("Pool/{}".format(instance), "*")
-    return [name for name in names if 'dserver' not in name]
+    return [name for name in names if "dserver" not in name]
 
 
 def get_ms_elements(instance, db):
     names = db.get_device_name("MacroServer/{}".format(instance), "*")
-    return [name for name in names if 'dserver' not in name]
+    return [name for name in names if "dserver" not in name]
+
 
 def generate_id_mapping(devices, db):
     id_map = unique_bidict()
     for name in devices:
-        prop = db.get_device_property(name, 'id')['id']
+        prop = db.get_device_property(name, "id")["id"]
         if not prop:
             continue
         id_map[prop[0]] = name
@@ -85,17 +85,19 @@ def generate_aliases_mapping(devices, db):
 
 def generate_instrument_list(pool, db):
     instr_list = []
-    instr_prop = db.get_device_property(pool, "instrumentlist")["instrumentlist"]
-    for n in range(0,len(instr_prop),3):
+    instr_prop = db.get_device_property(pool, "instrumentlist")[
+        "instrumentlist"
+    ]
+    for n in range(0, len(instr_prop), 3):
         instr_class = instr_prop[n]
-        instr_name = instr_prop[n+1]
-        instr_id = instr_prop[n+2]
+        instr_name = instr_prop[n + 1]
+        instr_id = instr_prop[n + 2]
         instr_list.append((instr_class, instr_name, instr_id))
     instr_list.sort(key=lambda a: a[2])
     return instr_list
 
 
-def generate_instrument_mapping(instr_list):
+:x
     id_map = unique_bidict()
     for instr in instr_list:
         id_map[instr[2]] = instr[1]
@@ -112,7 +114,6 @@ def generate_prop_mapping(devices, db, prop_name):
     return id_map
 
 
-
 def generate_class_mapping(devices, db):
     return {d: db.get_class_for_device(d) for d in devices}
 
@@ -122,25 +123,30 @@ if __name__ == "__main__":
     elements = get_elements("Femtomax", db)
     aliases = generate_aliases_mapping(elements, db)
     ids = generate_id_mapping(elements, db)
-    ctrl_ids = generate_prop_mapping(elements, db, 'ctrl_id')
-    motor_ids = generate_prop_mapping(elements, db, 'motor_role_ids')
-    pseudo_ids = generate_prop_mapping(elements, db, 'pseudo_motor_role_ids')
+    ctrl_ids = generate_prop_mapping(elements, db, "ctrl_id")
+    motor_ids = generate_prop_mapping(elements, db, "motor_role_ids")
+    pseudo_ids = generate_prop_mapping(elements, db, "pseudo_motor_role_ids")
     import networkx as nx
     import matplotlib.pyplot as plt
+
     # import pydot
     G = nx.Graph()
-    G.add_nodes_from(ids.values()
-                     )
+    G.add_nodes_from(ids.values())
 
     def add_edge(iterable):
         for name, elems in iterable.items():
             for e in elems:
                 G.add_edge(name, ids[e])
+
     add_edge(motor_ids)
     add_edge(pseudo_ids)
     add_edge(ctrl_ids)
 
-    nx.draw(G, node_size=100, cmap=plt.cm.Blues,
-            node_color=range(len(G)),
-            prog='dot',
-            with_labels=True)
+    nx.draw(
+        G,
+        node_size=100,
+        cmap=plt.cm.Blues,
+        node_color=range(len(G)),
+        prog="dot",
+        with_labels=True,
+    )
